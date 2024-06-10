4ae78616bf3377168243c1ef24738a617a4a1248
Vladimir Volek <volek.vladimir@gmail.com>
2024-06-10 03:07:37 +0200

    hm

--- mithril-client-wasm/src/client_wasm.rs
+++ mithril-client-wasm/src/client_wasm.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use http::HeaderMap;
+use http::{HeaderMap, HeaderName};
 use mithril_client::{
     feedback::{FeedbackReceiver, MithrilEvent},
     CardanoTransactionsProofs, Client, ClientBuilder, MessageBuilder, MithrilCertificate,
